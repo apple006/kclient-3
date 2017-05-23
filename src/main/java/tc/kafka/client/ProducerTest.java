07e6012e637c9b74335baa78464d06e0769c7704
ghst659 <ghst659@github.com>
2017-05-22 19:59:03 -0700

    Initial commit

---
+++ src/main/java/tc/kafka/client/ProducerTest.java
@@ -0,0 +1,32 @@
+package tc.kafka.client;
+// Based on https://howtoprogram.xyz/2016/05/02/apache-kafka-0-9-java-client-api-example/ 
+import java.util.Properties;
+ 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+ 
+public class ProducerTest {
+    public static void main(String[] args) {
+	Properties props = new Properties();
+	props.put("bootstrap.servers", "192.168.33.10:9092");
+	props.put("acks", "all");
+	props.put("retries", 0);
+	props.put("batch.size", 16384);
+	props.put("linger.ms", 1);
+	props.put("buffer.memory", 33554432);
+	props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+	props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+	try (Producer<String,String> producer = new KafkaProducer<>(props)) {
+	    for (int i = 0; i < 100; i++) {
+		String msg = "Message " + i;
+		producer.send(new ProducerRecord<String, String>("HelloKafkaTopic", msg));
+		System.out.println("Sent:" + msg);
+	    }
+	} catch (Exception e) {
+	    e.printStackTrace();
+	} finally {
+	    // producer.close();
+	}
+    }
+}
